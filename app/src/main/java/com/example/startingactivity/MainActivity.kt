62b466f6fa14f4e24371a1fe4751bd94502170e6
Sergey Vasilevkin <svasilevkin@gmail.com>
2020-02-29 12:40:23 +0300

    Add the second activity.

--- app/src/main/java/com/example/startingactivity/MainActivity.kt
+++ app/src/main/java/com/example/startingactivity/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.startingactivity
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
 
 import kotlinx.android.synthetic.main.activity_main.*
+
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,5 +41,16 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        // Do something in response to button
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
